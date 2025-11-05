d78e5ec714a18817dbc9425c79411691aea83ba3
zack-das <izackdas@gmail.com>
2025-11-05 11:41:20 +0300

    First commit

---
+++ schooldb.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+# Connect to database
+conn = sqlite3.connect('school.db')
+c = conn.cursor()
+
+c.execute('''
+CREATE TABLE Students(
+    student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    date_of_birth DATE NOT NULL
+)
+''')
+
+conn.commit()
+c.execute("SELECT sql FROM sqlite_master WHERE type='table' AND name='Students'")
+print("Table schema:")
+
+# Close connection
+c.close()
+conn.close()
